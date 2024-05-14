1f52903e91dbc1e16a8d39d6d92ab85517a07053
卄卂尺丂卄卂 <harsharajb14303@gamil.com>
2024-05-14 12:17:51 +0530

    first commit

--- crm.py
+++ crm.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from datetime import datetime
 from pymongo import MongoClient
 from bson import ObjectId
 from typing import List
- 
+
 app = FastAPI()
- 
+
 client =  MongoClient("mongodb+srv://harsha:harsha@cluster0.fny6tjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["crm"]
 users_collection = db["data"]
- 
+
 class UserCreate(BaseModel):
     email: str
     phone_number: int
@@ -24,7 +25,7 @@ class UserCreate(BaseModel):
     date_of_birth: str
     address: str = ""
     crmroles: str = ""
-    crmcreatedtime: str
+    crmcreatedtime: datetime = datetime.now()
     payment_information: str = ""
     order_history: str = ""
     preferences: str = ""
@@ -34,7 +35,7 @@ class UserCreate(BaseModel):
     reviews_and_ratings: str = ""
     customer_service_interactions: str = ""
     alternate_phone_number: int = None
- 
+
 @app.get("/", response_model=List[UserCreate])
 async def get_todos():
     todos = users_collection.find()
@@ -42,7 +43,7 @@ async def get_todos():
     for data in todos:
         result.append(data)
     return result
- 
+
 @app.get("/{usergroup}", response_model=List[UserCreate])
 async def get_users_by_usergroup(usergroup: str):
     users = users_collection.find({"usergroup": usergroup})
@@ -50,12 +51,12 @@ async def get_users_by_usergroup(usergroup: str):
     for user in users:
         result.append(user)
     return result
- 
- 
+
+
 @app.post("/")
-async def post_todo(data: UserCreate):
+async def post_data(data: UserCreate):
     users_collection.insert_one(dict(data))
-   
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
