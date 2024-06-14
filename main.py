6ca0fbaad1a28522a225625ad73fe890408f85d7
Abhiram k r <109401380+abhiram002@users.noreply.github.com>
2024-06-14 12:13:33 +0530

    Resolved merge conflicts

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from passlib.context import CryptContext # type: ignore
 # JWT settings
 SECRET_KEY = "crm"  # Change this to a strong secret key
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
  
 app = FastAPI()
  
